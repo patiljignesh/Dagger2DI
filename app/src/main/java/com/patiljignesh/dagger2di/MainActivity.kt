3f05fa3402aa4370f3d7093ffebaf37c063236ba
Jignesh Patil <patiljignesh@gmail.com>
2022-07-22 11:47:32 +0200

    Initial commit

---
+++ app/src/main/java/com/patiljignesh/dagger2di/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.patiljignesh.dagger2di
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
